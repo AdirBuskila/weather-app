11ff988ff6d6bf392c9ff19c805543cd53cc5bde
Adir Buskila <Adirbu98@gmail.com>
2022-02-05 23:04:36 +0200

    adding seacrh adding autocomplete adding support by timezone

---
+++ src/services/forecast.service.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+import {storageService} from './async-storage.service'
+const API_KEY = 'N4CxDgJ1zvPqW4hpfIkNgXAvuBfRzxiJ'
+
+export const forecastService = {
+    query,
+} 
+
+
+async function _checkStorage(searchedCity) {
+    const cities = await storageService.query('cities')
+    const res = cities.find((city) => city.name === searchedCity)
+    return res
+}
+
+
+const getCityKey = async (city) => {
+    const {data} = await axios.get(`http://dataservice.accuweather.com/locations/v1/search?apikey=${API_KEY}&q=${city}`)
+    const cityObj = {
+        key: data[0].Key,
+        name: data[0].LocalizedName,
+        timeZone: data[0].TimeZone.Name
+    }
+    return cityObj
+}
+
+
+async function query(searchedCity) {
+    let cityObjStorage = await _checkStorage(searchedCity)
+    if (!cityObjStorage) {
+        const city = await getCityKey(searchedCity)
+        const { data } = await axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${city.key}?apikey=${API_KEY}&metric=true`)
+        const cityObj = {
+            ...city,
+            dailyForecasts: data.DailyForecasts,
+            addedAt: Date.now(),
+            headline: data.Headline.Text
+        }
+        await storageService.post('cities', cityObj)
+        return cityObj
+    }
+    return cityObjStorage
+}
